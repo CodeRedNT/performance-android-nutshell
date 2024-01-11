9f0eb769603a470a579e4b72117f1682e353336d
Ranyele Amorim <rcode2001@gmail.com>
2024-01-10 23:14:48 -0300

    Update ExampleUnitTest.kt

--- app/src/test/java/com/coderednt/performance_android_nutshell/ExampleUnitTest.kt
+++ app/src/test/java/com/coderednt/performance_android_nutshell/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.coderednt.performance_android_nutshell
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
